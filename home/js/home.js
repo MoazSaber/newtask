64ad5f149fc324ed64c3d75a30603b8ea3c18ed6
MoazSaber <139745652+MoazSaber@users.noreply.github.com>
2023-08-08 12:21:18 +0300

    Update home.js

--- home/js/home.js
+++ home/js/home.js
@@ -1,4 +1,7 @@
 document.getElementById('userName').innerHTML= `Welcome ` + JSON.parse(localStorage.getItem('user'))
+if ( JSON.parse(localStorage.getItem('user')) == null){
+    document.getElementById('userName').innerHTML= `Welcome ` + "User"
+}
 
 
 let btn=document.getElementById('logBtn')
